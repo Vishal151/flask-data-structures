106ac4d167ffb556f82a24e34aa360b675c284c9
Vishal Patel <vishal.patel@sidewayseight.co.uk>
2021-08-09 12:51:06 +0100

    Added dummy data and linked list APIs

--- linked_list.py
+++ linked_list.py
@@ -8,6 +8,17 @@ class LinkedList:
       self.head = None
       self.last_node = None
 
+  def to_list(self):
+    l = []
+    if self.head is None:
+      return l
+
+    node = self.head
+    while node:
+      l.append(node.data)
+      node = node.next_node
+    return l
+
   def print_ll(self):
       ll_string = ""
       node = self.head
@@ -35,3 +46,11 @@ class LinkedList:
     
     self.last_node.next_node = Node(data, None)
     self.last_node = self.last_node.next_node
+
+  def get_user_by_id(self, user_id):
+    node = self.head
+    while node:
+      if node.data["id"] is int(user_id):
+        return node.data
+      node = node.next_node
+    return None
